74cec0c25404f309ac2d52ce64e4b12dd632e55c
Tzu-Chieh <aaa0613.cs07@nctu.edu.tw>
2020-07-09 17:08:43 +0800

    delete

---
+++ php/0716002_0716008/info.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<form action="query.php" method="post">
+<input type="submit" value="統計數據">
+</form>
+<form action="post.php" method="post">
+<input type="submit" value="條件查詢">
+</form>
+<form action="rate.php" method="post">
+<input type="submit" value="我要評分">
+</form>
+
+
+
+
+
+</body>
+</html>
