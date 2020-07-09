f1d4b0256f65207f0ed8602b250863d32479c9d4
peirulu <l890301@gmail.com>
2020-07-09 17:11:25 +0800

    check

---
+++ php/0716002_0716008/most_genre_oscar.php
@@ -0,0 +1,76 @@
+<table border="1">
+<tr>
+<?php
+$mysqli = new mysqli('localhost', 'root', '301850', 'project');
+
+// Oh no! A connect_errno exists so the connection attempt failed!
+if ($mysqli->connect_errno) {
+    echo "Sorry, this website is experiencing problems.";
+
+    echo "Error: Failed to make a MySQL connection, here is why: \n";
+    echo "Errno: " . $mysqli->connect_errno . "\n";
+    echo "Error: " . $mysqli->connect_error . "\n";
+    
+     exit;
+}
+
+// Perform an SQL query
+
+$sql="SELECT * from
+(SELECT gen.genre,count(*)as cnt
+from genre gen,
+(SELECT m.title,m.id 
+from movie m,
+(SELECT chart1.year,chart1.film,count(*) as cnt from
+(SELECT g.year,g.film,g.win from oscar g
+where g.win='TRUE')as chart1
+group by chart1.year,chart1.film)as chart2
+where m.title=chart2.film)as chart3
+where gen.id=chart3.id
+group by gen.genre)as table1
+order by table1.cnt DESC limit 3;";
+
+
+if (!$result = $mysqli->query($sql)) {
+    // Oh no! The query failed. 
+    echo "Sorry, the website is experiencing problems.";
+    echo "Error: Our query failed to execute and here is why: \n";
+    echo "Query: " . $sql . "\n";
+    echo "Errno: " . $mysqli->errno . "\n";
+    echo "Error: " . $mysqli->error . "\n";
+    exit;
+}
+
+// Phew, we made it. We know our MySQL connection and query 
+// succeeded, but do we have a result?
+if ($result->num_rows === 0) {
+    // Oh, no rows! Sometimes that's expected and okay, sometimes
+    // it is not. You decide. In this case, maybe actor_id was too
+    // large? 
+    echo "We could not find a match for ID $aid, sorry about that. Please try again.";
+    exit;
+}
+
+echo '<div style="font-size:1.25em;color:red">奧斯卡獎得獎數量最多的電影類型 </div>';
+$genre=Genre;
+$count='# of wins';
+
+echo '<tr><td>','#','</td>';
+echo '<td>',$genre,'</td>';
+echo '<td>',$count,'</td>';
+while ($actor = $result->fetch_assoc()) {    
+    $rowid = $rowid + 1;
+    echo '<tr><td>',$rowid,'</td>';
+    echo '<td>',$actor['genre'],'</td>';
+    echo '<td>',$actor['cnt'],'</td>';
+    
+
+}
+
+
+
+$result->free();
+$mysqli->close();
+?>
+</tr>
+</table>
---
+++ php/0716002_0716008/pity_movie_gg.php
@@ -0,0 +1,105 @@
+<table border="1">
+<tr>
+<?php
+$mysqli = new mysqli('localhost', 'root', '301850', 'project');
+
+// Oh no! A connect_errno exists so the connection attempt failed!
+if ($mysqli->connect_errno) {
+    echo "Sorry, this website is experiencing problems.";
+
+    echo "Error: Failed to make a MySQL connection, here is why: \n";
+    echo "Errno: " . $mysqli->connect_errno . "\n";
+    echo "Error: " . $mysqli->connect_error . "\n";
+    
+     exit;
+}
+
+// Perform an SQL query
+
+$sql="SELECT table1.year,table1.snub,table1.num_nominate
+from
+(SELECT chart4.year,min(chart4.film) as snub,chart4.cnt as num_nominate
+from
+(SELECT chart1.year,max(chart1.cnt) as cnt
+from
+(SELECT g.year,g.film,count(*) as cnt
+from golden_globe g 
+where win='FALSE'
+and g.film not in
+(SELECT temp.film
+from
+(SELECT g.year,g.film,g.win,count(*)
+from golden_globe g 
+where win='TRUE'
+group by year,film,win
+order by film)as temp)
+group by year,film,win
+order by year)as chart1
+group by year)as chart3
+,
+
+(SELECT g.year,g.film,count(*) as cnt
+from golden_globe g 
+where win='FALSE'
+and g.film not in
+(SELECT temp.film
+from
+(SELECT g.year,g.film,g.win,count(*)
+from golden_globe g 
+where win='TRUE'
+group by year,film,win
+order by film)as temp)
+group by year,film,win
+order by year)as chart4
+where chart3.year=chart4.year
+and chart3.cnt=chart4.cnt
+group by year
+order by year)as table1";
+
+
+
+
+if (!$result = $mysqli->query($sql)) {
+    // Oh no! The query failed. 
+    echo "Sorry, the website is experiencing problems.";
+    echo "Error: Our query failed to execute and here is why: \n";
+    echo "Query: " . $sql . "\n";
+    echo "Errno: " . $mysqli->errno . "\n";
+    echo "Error: " . $mysqli->error . "\n";
+    exit;
+}
+
+// Phew, we made it. We know our MySQL connection and query 
+// succeeded, but do we have a result?
+if ($result->num_rows === 0) {
+    // Oh, no rows! Sometimes that's expected and okay, sometimes
+    // it is not. You decide. In this case, maybe actor_id was too
+    // large? 
+    echo "We could not find a match for ID $aid, sorry about that. Please try again.";
+    exit;
+}
+
+echo '<div style="font-size:1.25em;color:red">金球獎最大遺珠 (每年沒得獎的電影中被提名次數最多的電影)</div>';
+$year=Year;
+$snub=Title;
+$num_nominate='# of Nominations';
+echo '<tr><td>','#','</td>';
+echo '<td>',$year,'</td>';
+echo '<td>',$snub,'</td>';
+echo '<td>',$num_nominate,'</td>';
+while ($actor = $result->fetch_assoc()) {    
+    $rowid = $rowid + 1;
+    echo '<tr><td>',$rowid,'</td>';
+    echo '<td>',$actor['year'],'</td>';
+    echo '<td>',$actor['snub'],'</td>';
+    echo '<td>',$actor['num_nominate'],'</td>';
+  
+}
+
+
+
+$result->free();
+$mysqli->close();
+?>
+</tr>
+</table>
